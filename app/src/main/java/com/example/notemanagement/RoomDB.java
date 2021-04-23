c91bb8c7ff6433be92acd1c4e68fb30fc386a86b
yenlee38 <ngocyen174308@gmail.com>
2021-04-24 06:58:13 +0700

    sua RoomDB, them NoteDAO, Note

--- app/src/main/java/com/example/notemanagement/RoomDB.java
+++ app/src/main/java/com/example/notemanagement/RoomDB.java
@@ -1,25 +1,32 @@
 package com.example.notemanagement;
 
 import android.content.Context;
+import android.renderscript.RenderScript;
 
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.notemanagement.DAO.AccountDAO;
+import com.example.notemanagement.DAO.CategoryDAO;
+import com.example.notemanagement.DAO.NoteDAO;
 import com.example.notemanagement.DAO.StatusDAO;
 import com.example.notemanagement.Entity.Account;
+import com.example.notemanagement.Entity.Category;
+import com.example.notemanagement.Entity.Note;
 import com.example.notemanagement.Entity.Status;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Account.class, Status.class},version = 1, exportSchema = false)
+@Database(entities = {Account.class, Note.class, Status.class, Category.class},version = 1, exportSchema = false)
 public abstract class RoomDB  extends RoomDatabase {
 
     //Create database instance
     public abstract AccountDAO accountDAO();
     public abstract StatusDAO statusDAO();
+    public abstract NoteDAO noteDAO();
+    public abstract CategoryDAO categoryDAO();
 
     private static String dbName = "notedatabase1";
     private static volatile RoomDB INSTANCE;
